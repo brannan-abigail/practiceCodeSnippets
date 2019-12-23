c3baf51237d4460734c9f1f9437a98bf2926bd08
brannan-abigail <brannan.abby@tamu.edu>
2019-12-22 19:29:07 -0600

    skeleton created

---
+++ Python/removeKFromList.py
@@ -0,0 +1,17 @@
+  
+# Python solution for removeKFromList (codeSignal problem). 
+
+class ListNode(object):
+   def __init__(self, x):
+     self.value = x
+     self.next = None
+
+def removeKFromList(l, k):
+    return l
+
+# initialize output as empty string
+output = []
+
+# call amendTheSentence with testing string (from one of the failed test cases on CodeSignal)
+output = removeKFromList([], '')
+print(output)
