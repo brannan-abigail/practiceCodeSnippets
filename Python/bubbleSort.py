0287c6982a27f149d93080ec52094f3dd04a2fbc
brannan-abigail <brannan.abby@tamu.edu>
2019-12-30 12:37:35 -0600

    added bubble sort solution

---
+++ Python/bubbleSort.py
@@ -0,0 +1,27 @@
+# Python solution for bubbleSort (codeSignal problem).
+# Most of the soln was provided, I needed to simply fill
+# in what length was.
+
+def bubbleSort(items):
+    def swap(firstIndex, secondIndex):
+        temp = items[firstIndex]
+        items[firstIndex] = items[secondIndex]
+        items[secondIndex] = temp
+
+    length = len(items)
+
+    for i in range(length):
+        j = 0
+        stop = length - i
+        while j < stop - 1:
+            if items[j] > items[j + 1]:
+                swap(j, j + 1)
+            j += 1
+    return items
+
+
+output = []
+
+items = [2, 4, 1, 5]
+output = bubbleSort(items)
+print(output)
